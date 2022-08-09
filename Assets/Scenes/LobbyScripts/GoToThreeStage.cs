aa31c24260218a356a81a5edf737918c83a88b0f
GApple427 <bogusjohann@naver.com>
2022-08-09 18:40:27 +0900

    stages
    GoToOneStage, GoToThreeStage, GoToTwoStage 스크립트 추가
    LobbyScripts폴더 추가후 거기에 로비하고 스테이지 씬에 필요한 스크립트 다 넣어뒀습니다

---
+++ Assets/Scenes/LobbyScripts/GoToThreeStage.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToThreeStage : MonoBehaviour
+{
+    public void GoThreeStage()
+    {
+        SceneManager.LoadScene("");//괄호 안에 세번째 스테이지 이름 추가
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
